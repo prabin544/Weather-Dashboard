fca00777798fdbe58c75c1758c3f8e94a88e6be3
Prabin Singh <pravin544@gmail.com>
2020-08-07 16:28:51 -0500

    Added png file for weather dashboard

--- Assets/script.js
+++ Assets/script.js
@@ -1,33 +1,36 @@
 $(document).ready(function(){
 
+    var currentDate = moment().format("YYYY.MM.DD HH:mm:ss");
+    console.log(currentDate);
+
     $("#searchBtn").click(function(){
         var cityName = $("#city").val();
         $("#city").empty();
-        console.log(cityName);
-        var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=9cb706456cc29e11df0385d8eb8de0f8";
+        var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=9cb706456cc29e11df0385d8eb8de0f8";
         $.ajax({
             url: weatherURL,
             method: "GET",
         }).then(function(response) {
-            var tempResults = response.list[0];
-            var lat = response.city.coord.lat;
-            var lon = response.city.coord.lon;
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
 
             getUV(lat, lon);
 
             var tempDiv = $("<div>");
-            var hCity = $("<h2>").text(response.city.name);
-            var pTemp = $("<p>").text("Temperature: " + tempResults.main.temp + " \u00B0F");
-            var pHum = $("<p>").text("Humidity: " + tempResults.main.humidity + " %");
-            var pWind = $("<p>").text("Humidity: " + tempResults.wind.speed + " MPH");
-            var pWind = $("<p>").text("Humidity: " + tempResults.wind.speed + " MPH");
+            weatherIcon(response);
+            var hCity = $("<h2>").text(response.name).addClass("wClass");
+            var pTemp = $("<p>").text("Temperature: " + response.main.temp + " \u00B0F");
+            var pHum = $("<p>").text("Humidity: " + response.main.humidity + " %");
+            var pWind = $("<p>").text("Humidity: " + response.wind.speed + " MPH");
+            var pWind = $("<p>").text("Humidity: " + response.wind.speed + " MPH");
             var pUIndex = $("<p>").text("UV Index: Loading....").addClass("uvClass");
-            // var iconurl = "http://openweathermap.org/img/w/" + pIcon + ".png";
-            // $('#wicon').attr('src', iconurl);
+            
             
             $("#temp-goes-here").empty();
             tempDiv.append(hCity, pTemp, pHum, pWind, pUIndex);
             $("#temp-goes-here").append(tempDiv);
+
+        fiveDayForecast(cityName);
             
         });
     });
@@ -44,11 +47,22 @@ $(document).ready(function(){
             });
         }
 
-        function weatherIcon(){
+        function weatherIcon(response){
+            var wiconCode = response.weather[0].icon;
+            console.log(wiconCode);
+            var iconurl = "http://openweathermap.org/img/w/" + wiconCode + ".png";
+            $("#wicon").attr('src', iconurl);
 
         }
 
-        function fiveDayForecast(){
+        function fiveDayForecast(cityName){
+            var fiveDayForecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=9cb706456cc29e11df0385d8eb8de0f8";
+            $.ajax({
+                url: fiveDayForecastUrl,
+                method: "GET",
+            }).then(function(response) {
+                console.log(response.list);
 
+            });
         }
 });
